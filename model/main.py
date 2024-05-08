01287d9e0a13930c04fb3974ced5c0977f97a828
JunseoHo <junseoho1029@gmail.com>
2024-05-08 22:16:16 +0900

    feat : model

---
+++ model/main.py
@@ -0,0 +1,17 @@
+from super_gradients.training import models
+import torch
+
+# 테스트 이미지 경로
+test_image_path = 'sample.jpg'
+
+# YOLONAS로 바운딩 박스 예측
+model = models.get(
+    model_name='yolo_nas_s',
+    checkpoint_path='./checkpoints/yolonas/ckpt_best.pth',
+    num_classes=2
+).to(torch.device("cuda:0") if torch.cuda.is_available() else torch.device("cpu"))
+
+out = model.predict('sample.jpg')
+
+image = out.image  # YOLONAS에 입력된 이미지 픽셀 값
+bboxes = out.prediction.bboxes_xyxy  # YOLONAS에서 예측된 바운딩 박스 값
--- trainer/yolonas/scripts/yolonas_tester.py
+++ trainer/yolonas/scripts/yolonas_tester.py
@@ -3,6 +3,7 @@ from tqdm.auto import tqdm
 import torch
 import os
 
+
 model_name = 'yolo_nas_s'
 pth_path = '../checkpoints/yolo_nas_s/RUN_20240508_175406_865478/ckpt_best.pth'
 classes = ['head', 'helmet']
@@ -19,6 +20,6 @@ test_images = os.listdir(test_imgs_dir)
 for test_image in tqdm(test_images, total=len(test_images)):
     test_image_path = os.path.join(test_imgs_dir, test_image)
     out = model.predict(test_image_path)
-    print(type(out))
-    print(out)
+    print(out.image)
+    print(out.prediction.bboxes_xyxy)
     out.save(os.path.join('../inference_results/', test_image))
