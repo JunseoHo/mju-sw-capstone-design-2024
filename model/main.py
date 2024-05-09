5693b37ed755a5a2362c616d32e4596bfe27379b
JunseoHo <junseoho1029@gmail.com>
2024-05-09 10:41:48 +0900

    feat : detected_objects

--- model/main.py
+++ model/main.py
@@ -1,6 +1,8 @@
 from super_gradients.training import models
 import torch
 
+from model.detected_object import DetectedObject
+
 # 테스트 이미지 경로
 test_image_path = 'sample.jpg'
 
@@ -13,7 +15,13 @@ model = models.get(
 
 out = model.predict('sample.jpg')
 
-image = out.image  # YOLONAS에 입력된 이미지 픽셀 값
-bboxes = out.prediction.bboxes_xyxy  # YOLONAS에서 예측된 바운딩 박스 값
+detected_objects = []
+for index in range(len(out.prediction.bboxes_xyxy)):
+    detected_objects.append(DetectedObject(out.image,
+                                           out.prediction.bboxes_xyxy[index],
+                                           out.prediction.confidence[index],
+                                           out.prediction.labels[index]))
 
-print(bboxes)
+for index, detected_object in enumerate(detected_objects):
+    detected_object.save(f'detected_object_{index}.png')
+
