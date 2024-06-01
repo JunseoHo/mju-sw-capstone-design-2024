bf6c5d1c9769b31f871a640a4010cf141206370c
JunseoHo <junseoho1029@gmail.com>
2024-06-01 17:10:54 +0900

    fix : mask alpha

--- trainer/unet3plus/scripts/une3plus_test.py
+++ trainer/unet3plus/scripts/une3plus_test.py
@@ -9,7 +9,7 @@ import numpy as np
 
 images_dir = '../test_dataset/images/'  # 전체 이미지가 저장되어 있는 디렉토리의 상대 주소
 json_path = '../test_dataset/annotations/instances_default.json'  # COCO Json 파일의 상대 주소
-h5_path = "../checkpoints/model.weights.h5"  # 학습된 모델의 저장 위치
+h5_path = "../checkpoints/epoch-61-val_loss-030.h5"  # 학습된 모델의 저장 위치
 input_size = (256, 256)  # 모델의 입력 크기
 num_of_image = 100
 cat_id = 4
@@ -40,12 +40,19 @@ for i in range(0, len(instance_images), batch_size):
     predicted_labels = [mask_to_label(prediction) for prediction in predictions]
 
     for j in range(len(batch_images)):
+        plt.figure(figsize=(10, 5))
+
+        # 원본 이미지 표시
         plt.subplot(1, 2, 1)
         plt.imshow(batch_images[j])
         plt.title('Original Image')
         plt.axis('off')
+
+        # 원본 이미지 위에 마스크를 겹침
         plt.subplot(1, 2, 2)
-        plt.imshow(predicted_labels[j], cmap='viridis')
-        plt.title('Predicted Labels')
+        plt.imshow(batch_images[j])
+        plt.imshow(predicted_labels[j], cmap='viridis', alpha=0.5)  # alpha 값으로 투명도 조절
+        plt.title('Original Image with Mask')
         plt.axis('off')
+
         plt.show()
